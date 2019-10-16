a6a6a87f6212548fb0eb0a4e21eb3162928a879b
757363985 <757363985@qq.com>
2019-10-16 18:52:28 +0800

    10.16 第二次上传

---
+++ DENGXI 10.16/server1 (2).js
@@ -0,0 +1,25 @@
+let express = require('express')
+let app = express()
+// 引入解析post模块
+let bodyParser = require('body-parser')
+let login = require('./router/login.js')
+let register = require('./router/register.js')
+let goods =require('./router/goods.js')
+let news = require('./router/news.js')
+let blogs = require('./router/blogs.js')
+let shopcar = require('./router/shopcar.js')
+let lendcodeParser = bodyParser.urlencoded({extended:false})
+
+app.all('*',function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*')
+    next()
+})
+
+app.use(express.static(__dirname + '/static'))
+app.post('/register',lendcodeParser,register.register)
+app.post('/login',lendcodeParser,login.login)
+app.get('/goods',goods.goodsinfo)
+app.get('/news',news.getnews)
+app.get('/blogs',blogs.getblogs)
+app.get('/shopcar',shopcar.getshopcar)
+app.listen(3001)
